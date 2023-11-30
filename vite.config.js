4f2005e35ee2ad35ec504ae45dbb95e2b8a92ade
Josué Dantas <105951027+wDarkRx@users.noreply.github.com>
2023-11-30 16:32:33 -0300

    add: gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/CombustivelWeb2/"
+  base: "/CombustivelWeb2",
+  plugins: [react()]
 })
